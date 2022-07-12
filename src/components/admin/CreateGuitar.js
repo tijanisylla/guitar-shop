d60aebc7d7cc0a4c84dad09e249a048cf3588687
tijani sylla <tijanisylla@Tijanis-MacBook-Pro.local>
2022-07-12 10:57:20 -0500

    life is good

--- src/components/admin/CreateGuitar.js
+++ src/components/admin/CreateGuitar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Create , SimpleForm, TextInput, DateInput} from 'react-admin'
+
  
 // --   model_name: "Stratocaster"
 // --   description: "Instantly, totally familiar..."
